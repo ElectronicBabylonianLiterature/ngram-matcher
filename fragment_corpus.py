0cfb17327382d814f225162c37e5a9b8e975ccc1
Fabian Simonjetz <fabian.simonjetz@rub.de>
2024-02-09 16:36:25 +0100

    add name param; parameterize threading

--- fragment_corpus.py
+++ fragment_corpus.py
@@ -20,27 +20,27 @@ class FragmentCorpus(BaseCorpus):
         n_values=DEFAULT_N_VALUES,
         show_progress=False,
         total=None,
+        threading=True,
+        name="",
     ):
-        super().__init__(n_values)
+        super().__init__(n_values, name=name)
 
         if show_progress and total is None:
             raise ValueError("If 'show_progress' is True, 'total' must be provided")
 
-        with ProcessPoolExecutor() as executor:
-            self.fragments = list(
-                tqdm(
-                    executor.map(
-                        partial(create_fragment_model, n_values=n_values), data
-                    ),
-                    total=total,
-                    desc="Building model",
-                    disable=not show_progress,
-                )
-            )
-        self._vocab = {
-            sign for fragment in self for ngram in fragment for sign in ngram
+        tqdm_config = {
+            "total": total,
+            "desc": "Building model",
+            "disable": not show_progress,
         }
-        self.documents = self.fragments
+
+        self.documents = self.fragments = (
+            self._load_threading if threading else self._load
+        )(data, tqdm_config)
+
+        self._vocab = {
+            sign for fragment in self for ngram in fragment.ngrams for sign in ngram
+        }
 
     @classmethod
     def load(
@@ -49,6 +49,8 @@ class FragmentCorpus(BaseCorpus):
         db="ebldev",
         uri=None,
         show_progress=True,
+        threading=True,
+        name="",
         **kwargs,
     ) -> "FragmentCorpus":
         query = {"signs": {"$regex": "."}}
@@ -63,12 +65,17 @@ class FragmentCorpus(BaseCorpus):
         if show_progress:
             total = get_total(query, cls._collection, db, uri)
             return cls(
-                data, n_values, show_progress, min(total, kwargs.get("limit", total))
+                data,
+                n_values,
+                show_progress,
+                min(total, kwargs.get("limit", total)),
+                name=name,
+                threading=threading,
             )
 
-        return cls(data, n_values)
+        return cls(data, n_values, name=name, threading=threading)
 
-    def compare(self, other, *n_values):
+    def match(self, other, *n_values):
         return [
             (
                 fragment.id_,
@@ -76,3 +83,18 @@ class FragmentCorpus(BaseCorpus):
             )
             for fragment in self.fragments
         ]
+
+    def _load(self, data: dict, tqdm_config: dict):
+        for entry in tqdm(data, **tqdm_config):
+            yield create_fragment_model(entry, self.n_values)
+
+    def _load_threading(self, data: dict, tqdm_config: dict):
+        with ProcessPoolExecutor() as executor:
+            return list(
+                tqdm(
+                    executor.map(
+                        partial(create_fragment_model, n_values=self.n_values), data
+                    ),
+                    **tqdm_config,
+                )
+            )
