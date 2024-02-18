cf3a9b77a0481cf0a06fe3be08df118dca16ee2d
Fabian Simonjetz <fabian.simonjetz@rub.de>
2024-02-18 16:51:36 +0100

    refactor

--- document_model.py
+++ document_model.py
@@ -18,6 +18,39 @@ class DocumentNotFoundError(Exception):
     pass
 
 
+def extract_ngrams(signs: pd.Series, n_values: Sequence[int]):
+    subframes = [
+        pd.concat([signs.shift(-i) for i in range(n)], axis=1)
+        .dropna()
+        .agg(tuple, axis=1)
+        for n in n_values
+    ]
+    ngrams = pd.concat(subframes)
+
+    return ngrams.drop_duplicates()
+
+
+def preprocess(raw_signs: str) -> pd.Series:
+    signs = pd.Series(raw_signs)
+
+    signs = signs.str.split("\n").explode().reset_index(drop=True)
+    signs = signs.str.strip()
+    signs.iloc[:-1] = signs.iloc[:-1].add(f" {LINE_SEP}")
+    signs = signs[~signs.str.fullmatch(rf"[{UNKNOWN_SIGN}{LINE_SEP}\s]*")]
+    signs = signs.str.split().explode()
+
+    return signs[signs.ne(UNKNOWN_SIGN)]
+
+
+def linewise_ngrams(signs: pd.Series, n_values: Sequence[int]) -> pd.Series:
+    return extract_ngrams(signs, n_values).reset_index(drop=True)
+
+
+def postprocess(signs: pd.Series):
+    signs = signs[~signs.map(set).map(lambda ngram: ngram <= {"X"})]
+    return set(signs)
+
+
 def fetch(
     query: dict,
     projection: dict,
@@ -58,7 +91,7 @@ class DocumentModel:
 
         return A & B
 
-    def similarity(self, other, *n_values):
+    def overlap_coefficient(self, other, *n_values):
         A = self.get_ngrams(*n_values)
         B = other.get_ngrams(*n_values)
 
@@ -118,36 +151,3 @@ class DocumentModel:
             )
 
         return model
-
-
-def extract_ngrams(signs: pd.Series, n_values: Sequence[int]):
-    subframes = [
-        pd.concat([signs.shift(-i) for i in range(n)], axis=1)
-        .dropna()
-        .agg(tuple, axis=1)
-        for n in n_values
-    ]
-    ngrams = pd.concat(subframes)
-
-    return ngrams.drop_duplicates()
-
-
-def preprocess(raw_signs: str) -> pd.Series:
-    signs = pd.Series(raw_signs)
-
-    signs = signs.str.split("\n").explode().reset_index(drop=True)
-    signs = signs.str.strip()
-    signs.iloc[:-1] = signs.iloc[:-1].add(f" {LINE_SEP}")
-    signs = signs[~signs.str.fullmatch(rf"[{UNKNOWN_SIGN}{LINE_SEP}\s]*")]
-    signs = signs.str.split().explode()
-
-    return signs[signs.ne(UNKNOWN_SIGN)]
-
-
-def linewise_ngrams(signs: pd.Series, n_values: Sequence[int]) -> pd.Series:
-    return extract_ngrams(signs, n_values).reset_index(drop=True)
-
-
-def postprocess(signs: pd.Series):
-    signs = signs[~signs.map(set).map(lambda ngram: ngram <= {"X"})]
-    return set(signs)
